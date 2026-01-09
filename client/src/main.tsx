de7294148aff89241f01050ff72343f2c58d6ab6
Priyanshi Govil <priyanshigovil@Priyanshis-MacBook-Air.local>
2026-01-09 20:03:34 +0530

    fix env and prepare for deploy

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,10 +4,18 @@ import App from "./App.tsx"
 import AppProvider from "./context/AppProvider.tsx"
 import "@/styles/global.css"
 
+
+// console.log("Backend URL:", import.meta.env.VITE_API_URL);
+// console.log("Backend URL 2:", import.meta.env.VITE_BACKEND_URL);
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
     // <React.StrictMode>
     <AppProvider>
         <App />
     </AppProvider>,
     // </React.StrictMode>
+
+
+
+    
 )
