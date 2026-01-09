9b0c9159cda502c7b3bfd779fc43eed679ec77f6
Priyanshi Govil <priyanshigovil@Priyanshis-MacBook-Air.local>
2026-01-09 23:36:24 +0530

    Fix homepage SVG overflow on production

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,27 +1,24 @@
 import illustration from "@/assets/illustration.svg"
 import FormComponent from "@/components/forms/FormComponent"
-// import Footer from "@/components/common/Footer";
 
 function HomePage() {
     return (
-      <div className="flex h-screen flex-col items-center justify-center overflow-hidden">
-
-
-          <div className="flex w-full max-w-6xl flex-col items-center justify-center gap-12 sm:flex-row">
-
-                <div className="flex w-full animate-up-down justify-center sm:w-1/2 sm:pl-4">
-                   <img
-  src={illustration}
-  alt="Code Sync Illustration"
-  className="mx-auto max-h-[70vh] w-auto sm:max-h-[60vh]"
-/>
-
+        <div className="flex min-h-screen flex-col items-center justify-center overflow-hidden py-12">
+            <div className="flex w-full max-w-6xl flex-col items-center justify-center gap-12 sm:flex-row">
+                
+                <div className="flex w-full justify-center sm:w-1/2 overflow-hidden">
+                    <img
+                        src={illustration}
+                        alt="Code Sync Illustration"
+                        className="mx-auto h-auto max-h-[50vh] max-w-full object-contain"
+                    />
                 </div>
+
                 <div className="flex w-full items-center justify-center sm:w-1/2">
                     <FormComponent />
                 </div>
+
             </div>
-            {/* <Footer /> */}
         </div>
     )
 }
